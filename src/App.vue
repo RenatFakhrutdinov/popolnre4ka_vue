1c9f1c29e0e8d7c646256f6f32621925853d636b
Renat <rfakhrutdinov1988@gmail.com>
2020-11-29 12:13:16 +1000

    add vue-router package

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,14 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
