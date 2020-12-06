f6cacb9f7e43ac1f1b012aada19967f3dd61083a
Renat <rfakhrutdinov1988@gmail.com>
2020-12-06 18:35:52 +1000

    create OperatorCard.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <router-view />
   </div>
 </template>
@@ -9,8 +8,7 @@
 <script>
 export default {
   name: 'App',
-  components: {
-  }
+  components: {}
 }
 </script>
 
---
+++ src/components/OperatorCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>{{operatorLabel}}</h2>
+        <p>{{pictPath}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "OperatorCard",
+        props: ['operatorLabel', 'pictPath'],
+        template: '<h2>{{operatorLabel}}</h2>'
+    }
+</script>
+
+<style scoped>
+
+</style>
